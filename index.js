4908367109ad47fac2d1bfe67a694bca157d61aa
Joh-Ishimwe <j.ishimwe3@alustudent.com>
2024-06-22 17:20:06 +0200

    updating

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ import allRouter from "./routes/user.routes.js";
 
 const app = express();
 
-const corsOptions = {
-  origin: [
-    "https://ecowaste-app-fh1j.onrender.com",
-    "http://localhost:5502"
-  ],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type, Authorization",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: [
+//     "https://ecowaste-app-fh1j.onrender.com",
+//     "http://localhost:5502"
+//   ],
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   allowedHeaders: "Content-Type, Authorization",
+//   credentials: true,
+// };
 
 app.use(cors(corsOptions));
 app.use(express.json());
